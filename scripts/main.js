3b8bd051b8ba099e23ace0d3521f55b64422572d
Ashrith Reddy <5827632+ashrithssreddy@users.noreply.github.com>
2024-09-27 05:11:26 -0500

    bug fixes

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ function loadSection(sectionName) {
         .then(data => {
             document.getElementById('content-container').innerHTML = data;
             updateActiveTab(sectionName);
+            attachCollapsibleEventListeners(); // Attach listeners after loading content
         })
         .catch(error => console.error('Error loading section:', error));
 }
@@ -16,6 +17,21 @@ function updateActiveTab(sectionName) {
             tab.classList.add('active');
         }
     }
+}
+
+function attachCollapsibleEventListeners() {
+    const collapsibleButtons = document.querySelectorAll('.collapsible');
+    collapsibleButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            this.classList.toggle('active-collapsible');
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
